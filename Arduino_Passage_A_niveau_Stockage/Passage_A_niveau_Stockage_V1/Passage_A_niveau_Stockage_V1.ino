eae1eaf11bc6c8121b5d0a2c9442580621ccf7d0
supermoun62 <supermoun62@gmail.com>
2024-03-12 09:42:11 +0100

    C. Mercier 12/03/2024 Modification schéma et programmes V1-V2 (Cde D11)

--- Arduino_Passage_A_niveau_Stockage/Passage_A_niveau_Stockage_V1/Passage_A_niveau_Stockage_V1.ino
+++ Arduino_Passage_A_niveau_Stockage/Passage_A_niveau_Stockage_V1/Passage_A_niveau_Stockage_V1.ino
@@ -20,18 +20,18 @@ Servo monServo2;
 int vitesse;
 int angle1;
 int angle2;
-const int angleMin1  = 1000;
-const int angleMax1 = 2000;
-const int angleMin2  = 1000;
-const int angleMax2 = 2000;
-const int ANGLE1SOUSTENSION = 1000;
-const int ANGLE2SOUSTENSION = 2000;
+const int ANGLE_MINI1  = 1000;
+const int ANGLE_MAX1 = 2000;
+const int ANGLE_MINI2  = 1000;
+const int ANGLE_MAX2 = 2000;
+const int ANGLE1_SOUSTENSION = 1000;
+const int ANGLE2_SOUSTENSION = 2000;
 bool cdeServos = 0;
 
 const int DET1 = A0;
 const int DET2 = A1;
 
-const byte LEDZONEOCCUPEE = 13;// Utilisee pour informtion zone occupee
+const byte LED_ZONE_OCCUPEE = 13;// Utilisee pour informtion zone occupee
 bool memDet1;
 bool memDet2;
 
@@ -50,26 +50,34 @@ const byte FLASH = 3;// Port PD
 const byte DEL_SW1_SW2 = 2;
 unsigned long compteurTempsLeds = 0; //  Variable pour compter le temps pour faire clignoter à intervale fixe les leds
 bool flipFlop = 0;// Allumage Leds
+
+unsigned long tempsCourant = 0;
+unsigned long tempsPULS_D11 = 500;
+const byte PULS_D11 = 11;// Pilotage circuit son
+bool memPulsD11 = 0; 
+
 //-----------------------------------------------------------------
 void setup()
 {
 monServo1.attach(4);// Port PD
 monServo2.attach(8);// Port PB
-angle1 = angleMin1;
-angle2 = angleMin2;
+angle1 = ANGLE_MINI1;
+angle2 = ANGLE_MINI2;
 vitesse = 2;
-monServo1.writeMicroseconds(ANGLE1SOUSTENSION);
-monServo2.writeMicroseconds(ANGLE2SOUSTENSION);
+monServo1.writeMicroseconds(ANGLE1_SOUSTENSION);
+monServo2.writeMicroseconds(ANGLE2_SOUSTENSION);
 
 pinMode(DET1, INPUT);// Detecteur avant passage a niveau
 pinMode(DET2, INPUT);// Detecteur apres passage a niveau
 pinMode(FLASH, OUTPUT);
 pinMode(DEL_SW1_SW2, OUTPUT);
-pinMode(LEDZONEOCCUPEE, OUTPUT);
+pinMode(LED_ZONE_OCCUPEE, OUTPUT);
+pinMode(PULS_D11, OUTPUT);
 
 digitalWrite(FLASH,HIGH);
 digitalWrite(DEL_SW1_SW2,HIGH); 
-digitalWrite(LEDZONEOCCUPEE,LOW);
+digitalWrite(LED_ZONE_OCCUPEE,LOW);
+digitalWrite(PULS_D11, HIGH);
 memDet1 = 0;
 memDet2 = 1;
 
@@ -128,12 +136,12 @@ etatDet2 = analogRead(DET2);
 //--------------------------------------------
   if(zoneOccupee == 1) 
     {  
-      digitalWrite(LEDZONEOCCUPEE,HIGH);
-      Signal();// appel fonction Leds
+      digitalWrite(LED_ZONE_OCCUPEE,HIGH);
+      Signal();// appel fonction Leds      
     }      
   else
     {
-      digitalWrite(LEDZONEOCCUPEE,LOW);
+      digitalWrite(LED_ZONE_OCCUPEE,LOW);
       digitalWrite(FLASH,HIGH);               
     }   
 //--------------------------------------------
@@ -160,9 +168,11 @@ else
     digitalWrite(DEL_SW1_SW2,HIGH);
   }
 //--------------------------------------------
-Serial.print(etatDet1);
-Serial.print("\t");
-Serial.println(etatDet2);
+pulsD11();
+//--------------------------------------------    
+//Serial.print(etatDet1);
+//Serial.print("\t");
+//Serial.println(etatDet2);
 //Serial.print("\t");
 //Serial.print(memDet1);
 //Serial.print("\t");
@@ -184,9 +194,9 @@ monServo1.writeMicroseconds(angle1);
 /* calcule le nouvel angle */
 angle1 = angle1 + vitesse;
  
-  if (angle1 > angleMax1)
+  if (angle1 > ANGLE_MAX1)
     {
-      angle1 = angleMax1;
+      angle1 = ANGLE_MAX1;
     }
 //FLASHay(2);    
 }  
@@ -197,9 +207,9 @@ monServo1.writeMicroseconds(angle1);
 /* calcule le nouvel angle */
 angle1 = angle1 - vitesse;
 
-  if (angle1 < angleMin1)
+  if (angle1 < ANGLE_MINI1)
     {
-      angle1 = angleMin1;
+      angle1 = ANGLE_MINI1;
     }
 //FLASHay(2);    
 }  
@@ -210,9 +220,9 @@ monServo2.writeMicroseconds(angle2);
 /* calcule le nouvel angle */
 angle2 = angle2 + vitesse;
  
-  if (angle2 > angleMax2)
+  if (angle2 > ANGLE_MAX2)
     {
-      angle2 = angleMax2;
+      angle2 = ANGLE_MAX2;
     }
 //FLASHay(2);    
 }  
@@ -223,9 +233,9 @@ monServo2.writeMicroseconds(angle2);
 /* calcule le nouvel angle */
 angle2 = angle2 - vitesse;
 
-  if (angle2 < angleMin2)
+  if (angle2 < ANGLE_MINI2)
     {
-      angle2 = angleMin2;      
+      angle2 = ANGLE_MINI2;      
     }
 //FLASHay(2);    
 } 
@@ -247,3 +257,20 @@ void Signal()
          }
     }
 } 
+void pulsD11()
+{
+  if(zoneOccupee == 1 && memPulsD11 == 0)
+  {
+    digitalWrite(PULS_D11, LOW);
+    memPulsD11 = 1;
+    tempsCourant = millis();
+  }  
+if(millis() - tempsCourant >= tempsPULS_D11)
+  {
+    digitalWrite(PULS_D11, HIGH);         
+  }      
+if(zoneOccupee == 0)
+  {
+    memPulsD11 = 0;    
+  }  
+}
--- Arduino_Passage_A_niveau_Stockage/Passage_A_niveau_Stockage_V2/Passage_A_niveau_Stockage_V2.ino
+++ Arduino_Passage_A_niveau_Stockage/Passage_A_niveau_Stockage_V2/Passage_A_niveau_Stockage_V2.ino
@@ -20,19 +20,19 @@ Servo monServo2;
 int vitesse;
 int angle1;
 int angle2;
-const int angleMin1  = 1300;//  Modifie le 26/12/2023
-const int angleMax1 = 2000;
-const int angleMin2  = 1300;//  Modifie le 26/12/2023
-const int angleMax2 = 2000;
-const int ANGLE1SOUSTENSION = 2000;//  Modifie le 26/12/2023
-const int ANGLE2SOUSTENSION = 2000;//  Modifie le 26/12/2023
+const int ANGLE_MIN1  = 1300;//  Modifie le 26/12/2023
+const int ANGLE_MAX1 = 2000;
+const int ANGLE_MIN2  = 1300;//  Modifie le 26/12/2023
+const int ANGLE_MAX2 = 2000;
+const int ANGLE1_SOUSTENSION = 2000;//  Modifie le 26/12/2023
+const int ANGLE2_SOUSTENSION = 2000;//  Modifie le 26/12/2023
 
 bool cdeServos;
 
 const int DET1 = A0;
 const int DET2 = A1;
 
-const byte LEDZONEOCCUPEE = 13;// Utilisee pour informtion zone occupee
+const byte LED_ZONE_OCCUPEE = 13;// Utilisee pour informtion zone occupee
 bool memDet1;
 bool memDet2;
 
@@ -51,26 +51,33 @@ const byte FLASH= 3;// Port PD
 const byte DEL_SW1_SW2 = 2;
 unsigned long compteurTempsLeds = 0; //  Variable pour compter le temps pour faire clignoter à intervale fixe les leds
 bool flipFlop = 0;// Allumage Leds
+
+unsigned long tempsCourant = 0;
+unsigned long tempsPULS_D11 = 500;
+const byte PULS_D11 = 11;// Pilotage circuit son
+bool memPulsD11 = 0; 
 //--------------------------------------------
 void setup()
 {
 monServo1.attach(4);// Port PD
 monServo2.attach(8);// Port PB
-angle1 = angleMin1;
-angle2 = angleMin2;
+angle1 = ANGLE_MIN1;
+angle2 = ANGLE_MIN2;
 vitesse = 2;
-monServo1.writeMicroseconds(ANGLE1SOUSTENSION);
-monServo2.writeMicroseconds(ANGLE2SOUSTENSION);
+monServo1.writeMicroseconds(ANGLE1_SOUSTENSION);
+monServo2.writeMicroseconds(ANGLE2_SOUSTENSION);
 
 pinMode(DET1, INPUT);// Detecteur avant passage a niveau
 pinMode(DET2, INPUT);// Detecteur apres passage a niveau
 pinMode(FLASH, OUTPUT);
 pinMode(DEL_SW1_SW2, OUTPUT);
-pinMode(LEDZONEOCCUPEE, OUTPUT);
+pinMode(LED_ZONE_OCCUPEE, OUTPUT);
+pinMode(PULS_D11, OUTPUT);
 
 digitalWrite(FLASH,HIGH);
 digitalWrite(DEL_SW1_SW2,HIGH);
-digitalWrite(LEDZONEOCCUPEE,LOW);
+digitalWrite(LED_ZONE_OCCUPEE,LOW);
+digitalWrite(PULS_D11, HIGH);
 memDet1 = 0;
 memDet2 = 1;
 
@@ -127,12 +134,12 @@ etatDet2 = analogRead(DET2);
 //--------------------------------------------
   if(zoneOccupee == 1) 
     {  
-      digitalWrite(LEDZONEOCCUPEE,HIGH);
+      digitalWrite(LED_ZONE_OCCUPEE,HIGH);
       Signal();// appel fonction Leds
     }      
   else
     {
-      digitalWrite(LEDZONEOCCUPEE,LOW);
+      digitalWrite(LED_ZONE_OCCUPEE,LOW);
       digitalWrite(FLASH,HIGH);
     } 
 //--------------------------------------------
@@ -162,6 +169,7 @@ else
   {
     digitalWrite(DEL_SW1_SW2,HIGH);
   }
+pulsD11();  
 //--------------------------------------------
 //Serial.print(etatDet1);
 //Serial.print("\t");
@@ -187,9 +195,9 @@ monServo1.writeMicroseconds(angle1);
 /* calcule le nouvel angle */
 angle1 = angle1 + vitesse;
  
-  if (angle1 > angleMax1)
+  if (angle1 > ANGLE_MAX1)
     {
-      angle1 = angleMax1;
+      angle1 = ANGLE_MAX1;
     }
 //delay(2);    
 }  
@@ -200,9 +208,9 @@ monServo1.writeMicroseconds(angle1);
 /* calcule le nouvel angle */
 angle1 = angle1 - vitesse;
 
-  if (angle1 < angleMin1)
+  if (angle1 < ANGLE_MIN1)
     {
-      angle1 = angleMin1;
+      angle1 = ANGLE_MIN1;
     }
 //delay(2);    
 }  
@@ -213,9 +221,9 @@ monServo2.writeMicroseconds(angle2);
 /* calcule le nouvel angle */
 angle2 = angle2 + vitesse;
  
-  if (angle2 > angleMax2)
+  if (angle2 > ANGLE_MAX2)
     {
-      angle2 = angleMax2;
+      angle2 = ANGLE_MAX2;
     }
 //delay(2);    
 }  
@@ -226,9 +234,9 @@ monServo2.writeMicroseconds(angle2);
 /* calcule le nouvel angle */
 angle2 = angle2 - vitesse;
 
-  if (angle2 < angleMin2)
+  if (angle2 < ANGLE_MIN2)
     {
-      angle2 = angleMin2;
+      angle2 = ANGLE_MIN2;
     }
 //delay(2);    
 }  
@@ -249,4 +257,21 @@ void Signal()
           digitalWrite(FLASH,LOW);          
          }
     }
+}
+void pulsD11()
+{
+  if(zoneOccupee == 1 && memPulsD11 == 0)
+  {
+    digitalWrite(PULS_D11, LOW);
+    memPulsD11 = 1;
+    tempsCourant = millis();
+  }  
+if(millis() - tempsCourant >= tempsPULS_D11)
+  {
+    digitalWrite(PULS_D11, HIGH);         
+  }      
+if(zoneOccupee == 0)
+  {
+    memPulsD11 = 0;    
+  }  
 }
